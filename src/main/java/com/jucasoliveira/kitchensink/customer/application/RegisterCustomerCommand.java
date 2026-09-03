862fda35bb3cffe90366c7099fa59579d2e1f438
jucasoliveira <jucas.oliveira@gmail.com>
2026-09-03 11:13:02 +0100

    fix(customer): replace jakarta.annotation.Nonnull with jakarta.validation.constraints.NotNull in RegisterCustomerCommand

--- src/main/java/com/jucasoliveira/kitchensink/customer/application/RegisterCustomerCommand.java
+++ src/main/java/com/jucasoliveira/kitchensink/customer/application/RegisterCustomerCommand.java
@@ -2,13 +2,13 @@ package com.jucasoliveira.kitchensink.customer.application;
 
 import com.jucasoliveira.kitchensink.customer.domain.ContactInfo;
 
-import jakarta.annotation.Nonnull;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterCustomerCommand(@NotBlank @Size(max = 25) @Pattern(regexp = "[^%*]*") String userId,
-        @NotBlank @Size(max = 25) String password, @Nonnull @Valid ContactInfo contactInfo) {
+        @NotBlank @Size(max = 25) String password, @NotNull @Valid ContactInfo contactInfo) {
 
 }
